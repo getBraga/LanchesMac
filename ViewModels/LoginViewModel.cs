bef567702ea0f0c3c404df064efa337ac61f7999
Ysmael Braga <ysmael.braga@ibm.com>
2022-11-18 00:14:32 -0300

    retirando o excesso de espaços que podem tentar ser colocado pelo usuario para permiter apenas um espaço entre o nome composto

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -1,14 +1,34 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace LanchesMac.ViewModels
 {
     public class LoginViewModel
     {
+       
+       
+        private string _name;
         [Required(ErrorMessage = "Informe o Nome")]
-        [Display(Name ="Usuário")]
-        public string UserName { get; set; }
+        [Display(Name = "Usuário")]
+     
+        public string UserName
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (_name != null && _name.Equals(value))
+                {
+                    return;
+                }
+                _name ??= Regex.Replace(value, @"\s+", " ");
+                
 
-        
+            }
+        }
+
         [Display(Name = "Endereço")]
         public string Endereco { get; set; }
 
